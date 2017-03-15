9b9e261d09d032a32c308b9c91a931f3ad269b5a
Axel <lezinaxel@gmail.com>
2017-03-15 16:47:48 +0100

    Liste + input mis à jour

--- src/Items.js
+++ src/Items.js
@@ -3,22 +3,33 @@ import React, { Component } from 'react';
 class Items extends Component {
   constructor() {
     super();
+    this.state = {
+      items : [
+        {
+          id : 1,
+          content : "coder"
+        },
+        {
+          id : 2,
+          content : "trouver une copine"
+        }
+      ]
+    }
   }
   render(){
-    let items = ["jouer", "coder", "manger", "boire"];
-    let itemsNodes = items.map((item)=>
-    <li key={item.toString()}>{item}</li>
-  );
-  return(
-    <ul>{itemsNodes}</ul>
-  );
+    const itemsNodes = this.state.items.map(item =>{
+      console.log(itemsNodes);
+      return <li key={ item.id }>{ item.content }</li>
+    });
+    return (
+      <div>
+        <ul>
+          { itemsNodes }
+        </ul>
+      </div>    
+    )
   }
 }
-
-
-
-
-
 
 
 
