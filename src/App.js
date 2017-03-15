9e58edca6c712666ebe2c7bde02a35e5615177df
Axel <lezinaxel@gmail.com>
2017-03-15 14:26:45 +0100

    Input pour rentrer les tâches

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Input from './Input.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <h3>ToDO List </h3>
-      <label for="task"/>
-      <input name="task"/>
+      <Input/>
       </div>
     );
   }
---
+++ src/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import './App.js';
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+      <input type="text"/>
+      </div>
+    )
+  }
+}
+
+export default Input;
